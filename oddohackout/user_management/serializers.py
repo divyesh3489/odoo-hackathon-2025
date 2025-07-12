61ddee819f469bf4df4989d25f67ea16cbdc51b5
shivam <shivp72019@gmail.com>
2025-07-12 10:51:38 +0530

    AVAILIBILITY ADDED

--- oddohackout/user_management/serializers.py
+++ oddohackout/user_management/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
+from utils.constatnt import AvailabilityConstants
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
      def validate(self,attrs):
@@ -18,10 +19,48 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         return data
 
 class UserSerializer(serializers.ModelSerializer):
+    availability = serializers.ListField(
+        child=serializers.CharField(),
+        required=False,
+        allow_empty=True,
+        help_text="Select multiple availability options"
+    )
+    
     class Meta:
         model = get_user_model()
-        fields = ['id','email','username','first_name','last_name','profile_image','password']
+        fields = ['id','email','username','first_name','last_name','profile_image','password', 'availability']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def validate_availability(self, value):
+        """Validate that availability contains only valid choices"""
+        if value:
+            valid_choices = [
+                AvailabilityConstants.WEEKDAYS,
+                AvailabilityConstants.WEEKENDS,
+                AvailabilityConstants.EVEINGS,
+                AvailabilityConstants.MORNINGS,
+                AvailabilityConstants.AFTERNOONS,
+                AvailabilityConstants.NIGHTS,
+                AvailabilityConstants.MONDAYS,
+                AvailabilityConstants.TUESDAYS,
+                AvailabilityConstants.WEDNESDAYS,
+                AvailabilityConstants.THURSDAYS,
+                AvailabilityConstants.FRIDAYS,
+                AvailabilityConstants.SATURDAYS,
+                AvailabilityConstants.SUNDAYS,
+            ]
+            
+            for item in value:
+                if item not in valid_choices:
+                    raise serializers.ValidationError(f"'{item}' is not a valid availability choice")
+                    
+            # Remove duplicates while preserving order
+            unique_values = []
+            for item in value:
+                if item not in unique_values:
+                    unique_values.append(item)
+            return unique_values
+        return value
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
